7e6ea02aa88a5f86ab804093c8638dd929d3a88a
sandrorvr <sandrorvr@yahoo.com.br>
2023-03-23 12:00:08 -0300

    update

--- backend/sefit/admin.py
+++ backend/sefit/admin.py
@@ -4,42 +4,42 @@ from .models import Servidores, Operacao, Lotacao, Local, EscMensal, EscOperacao
 
 @admin.register(Servidores)
 class ServidoresAdmin(admin.ModelAdmin):
-    list_display = ('maticula', 'nome')
+    list_display = ('mat', 'name')
 
 @admin.register(Operacao)
 class OperacaoAdmin(admin.ModelAdmin):
-    list_display = ['name_operacao']
+    list_display = ['name']
 
 @admin.register(Lotacao)
 class LotacaoAdmin(admin.ModelAdmin):
-    list_display = ['name_lotacao']
+    list_display = ['name']
 
 @admin.register(Local)
 class LocalAdmin(admin.ModelAdmin):
-    list_display = ['name_local']
+    list_display = ['name']
     
 @admin.register(EscMensal)
 class EscMensalAdmin(admin.ModelAdmin):
-    list_display = ('mat', 'cargo', 'lotacao', 
-                    'local', 'data_mensal', 'hora_inicio', 
-                    'hora_termino', 'equipamento'
+    list_display = ('mat', 'function', 'lotacao', 
+                    'local', 'dt_esc_m', 'h_in', 
+                    'h_out', 'eqp'
                     )
 
 @admin.register(EscOperacao)
 class EscOperacaoAdmin(admin.ModelAdmin):
-    list_display = ('mat', 'cargo', 'local',
-                    'data_operacao', 'hora_inicio', 'hora_termino', 
-                    'roteiro', 'operacao'
+    list_display = ('mat', 'function', 'local',
+                    'dt_op', 'h_in', 'h_out', 
+                    'road_map', 'operacao'
                     )
 
 @admin.register(Permutas)
 class PermutasAdmin(admin.ModelAdmin):
-    list_display = ('escalado', 'substituto', 'data_permuta', 'data_criacao')
+    list_display = ('selected', 'not_selected', 'dt_swap', 'dt_creation')
 
 @admin.register(Faltas)
 class FaltasAdmin(admin.ModelAdmin):
-    list_display = ('mat', 'data_falta', 'data_criacao')
+    list_display = ('mat', 'dt_missing', 'dt_creation')
 
 @admin.register(Pedidos)
 class PedidosAdmin(admin.ModelAdmin):
-    list_display = ('mat', 'tipo', 'data_pedido', 'data_criacao', 'status')
+    list_display = ('mat', 'tipo', 'dt_pedido', 'dt_creation', 'status')
--- backend/sefit/models.py
+++ backend/sefit/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 class Servidores(models.Model):
-    mat = models.IntegerField(name='maticula', primary_key=True)
-    name = models.CharField(name='nome', max_length=45)
+    mat = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45)
 
     def __str__(self):
         return self.name
     
 class Operacao(models.Model):
-    name = models.CharField(name='name_operacao', max_length=25)
+    name = models.CharField(max_length=25)
     desc = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Lotacao(models.Model):
-    name = models.CharField(name='name_lotacao', max_length=25)
+    name = models.CharField(max_length=25)
     boss = models.ForeignKey(Servidores, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Local(models.Model):
-    name = models.CharField(name='name_local', max_length=25)
+    name = models.CharField(max_length=25)
     desc = models.TextField()
 
     def __str__(self):
@@ -30,54 +30,54 @@ class Local(models.Model):
 
 class EscMensal(models.Model):
     mat = models.ForeignKey(Servidores, on_delete=models.CASCADE)
-    function = models.CharField(name='cargo', max_length=30)
+    function = models.CharField(max_length=30)
     lotacao = models.ForeignKey(Lotacao, on_delete=models.CASCADE)
     local = models.ForeignKey(Local, on_delete=models.CASCADE)
-    dt_esc_m = models.DateField(name='data_mensal')
-    h_in = models.TimeField(name='hora_inicio')
-    h_out = models.TimeField(name='hora_termino')
-    eqp = models.CharField(name='equipamento', max_length=20)
+    dt_esc_m = models.DateField()
+    h_in = models.TimeField()
+    h_out = models.TimeField()
+    eqp = models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.mat.mat} - {str(self.dt_esc_m)}'
 
 class EscOperacao(models.Model):
     mat = models.ForeignKey(Servidores, on_delete=models.CASCADE)
-    function = models.CharField(name='cargo', max_length=30)
+    function = models.CharField(max_length=30)
     local = models.ForeignKey(Local, on_delete=models.CASCADE)
-    dt_op = models.DateField(name='data_operacao')
-    h_in = models.TimeField(name='hora_inicio')
-    h_out = models.TimeField(name='hora_termino')
-    road_map = models.CharField(name='roteiro', max_length=5)
+    dt_op = models.DateField()
+    h_in = models.TimeField()
+    h_out = models.TimeField()
+    road_map = models.CharField(max_length=5)
     operacao = models.ForeignKey(Operacao, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.operacao.name} - {str(self.dt_op)}'
 
 class Permutas(models.Model):
-    selected = models.IntegerField(name='escalado')
-    not_selected = models.IntegerField(name='substituto')
-    dt_swap = models.DateField(name='data_permuta')
-    dt_creation = models.DateField(name='data_criacao', auto_now=True)
+    selected = models.IntegerField()
+    not_selected = models.IntegerField()
+    dt_swap = models.DateField()
+    dt_creation = models.DateField(auto_now=True)
 
     def __str__(self):
         return f'{self.selected.name} - {self.not_selected.name}'
 
 class Faltas(models.Model):
     mat = models.ForeignKey(Servidores, on_delete=models.CASCADE)
-    dt_missing= models.DateField(name='data_falta')
-    dt_creation = models.DateField(name='data_criacao', auto_now=True)
+    dt_missing= models.DateField()
+    dt_creation = models.DateField(auto_now=True)
 
     def __str__(self):
         return f'{self.mat} - {str(self.dt_creation)}'
 
 class Pedidos(models.Model):
     mat = models.ForeignKey(Servidores, on_delete=models.CASCADE)
-    tipo = models.CharField(name='tipo', max_length=10)
+    tipo = models.CharField(max_length=10)
     desc = models.TextField()
-    dt_pedido = models.DateField(name='data_pedido')
-    dt_creation = models.DateField(name='data_criacao', auto_now=True)
-    status = models.BooleanField(name='status')
+    dt_pedido = models.DateField()
+    dt_creation = models.DateField(auto_now=True)
+    status = models.BooleanField()
 
     def __str__(self):
         return f'{self.mat} - {self.tipo}'
