16bb78b93ea3fde2e0f619ebe1d61d0b5d02dc09
sandrorvr <sandrorvr@yahoo.com.br>
2023-03-28 11:50:23 -0300

    modificated front-end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,22 @@ import './App.css'
 import BoxOfWorkers from './components/boxOfWorkers';
 import Modal from './components/modal';
 import ModalForm from './components/modal/modalForm';
+import ChartBar from './components/ChartBar';
+
 
 function App() {
- 
+  
+
   return (
     <div className="App">
       <section className='container'>
-        <section className='side-bar'>
-
-        </section>
-        <section className='content'>
-          <BoxOfWorkers>
+        <section className='side-bar'></section>
+          <BoxOfWorkers className='findWorkers'>
             <ModalForm/>
           </BoxOfWorkers>
-        </section>
+          <ChartBar className='c0'></ChartBar>
+          <ChartBar className='c1'></ChartBar>
+          <ChartBar className='c2'></ChartBar>
       </section>
 
     </div>
